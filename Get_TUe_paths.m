f010c06a5e84b46dc79b637f6e85e2f6fb119d45
Rodrigo García <yo@rodrigogarcia.me>
2015-04-01 16:38:50 +0200

    Refactors path constants and adds Thesis path

--- Get_TUe_paths.m
+++ Get_TUe_paths.m
@@ -1,4 +1,7 @@
 function paths = Get_TUe_paths
-    MATLAB_shared       = ['/Users/rodrigo/Documents/MATLAB/MATLAB_shared' delim];
+    MATLAB_base         = ['/Users/rodrigo/Documents/MATLAB' delim];
+    MATLAB_shared       = [MATLAB_base 'MATLAB_shared' delim];
+    
+    paths.Thesis        = [MATLAB_base 'Thesis' delim];
     paths.PsySound32    = [MATLAB_shared 'tb_PsySound32' delim];
 end
