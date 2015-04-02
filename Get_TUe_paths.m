fbe3fde7650e28af5c31954861f2bd268cff7119
Rodrigo García <yo@rodrigogarcia.me>
2015-04-02 12:15:39 +0200

    Adds DSP to path

--- Get_TUe_paths.m
+++ Get_TUe_paths.m
@@ -1,7 +1,8 @@
 function paths = Get_TUe_paths
     MATLAB_base         = ['/Users/rodrigo/Documents/MATLAB' delim];
     MATLAB_shared       = [MATLAB_base 'MATLAB_shared' delim];
-    
+
+    paths.DSP           = [MATLAB_shared 'DSP' delim];
     paths.Thesis        = [MATLAB_base 'Thesis' delim];
     paths.PsySound32    = [MATLAB_shared 'tb_PsySound32' delim];
 end
