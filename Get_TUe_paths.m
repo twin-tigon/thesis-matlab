c8aa3ec1b592efe774c2ab34903daf5e47bdecab
Rodrigo García <yo@rodrigogarcia.me>
2015-04-21 15:30:32 +0200

    Update paths regarding the merger of repositories

--- Get_TUe_paths.m
+++ Get_TUe_paths.m
@@ -1,10 +1,9 @@
 function paths = Get_TUe_paths
     MATLAB_base         = ['/Users/rodrigo/Documents/MATLAB' delim];
     MATLAB_shared       = [MATLAB_base 'MATLAB_shared' delim];
-    Thesis              = [MATLAB_base 'Thesis' delim];
+    Psychoacoustics     = [MATLAB_shared 'Psychoacoustics' delim];
 
     paths.DSP                   = [MATLAB_shared 'DSP' delim];
 %     paths.PsySound32            = [MATLAB_shared 'tb_PsySound32' delim];
-    
-    paths.FluctuationStrength   = [Thesis 'FluctuationStrength' delim];
+    paths.FluctuationStrength   = [Psychoacoustics 'FluctuationStrength_Rodrigo' delim];
 end
