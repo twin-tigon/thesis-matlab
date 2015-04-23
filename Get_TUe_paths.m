7e24e0df12f6d84528d669661e4c9c37a610037c
Rodrigo García <yo@rodrigogarcia.me>
2015-04-23 17:02:08 +0200

    Adds a space to differentiate paths in Get_TUe_paths

--- Get_TUe_paths.m
+++ Get_TUe_paths.m
@@ -5,5 +5,7 @@ function paths = Get_TUe_paths
 
     paths.DSP                   = [MATLAB_shared 'DSP' delim];
 %     paths.PsySound32            = [MATLAB_shared 'tb_PsySound32' delim];
+
     paths.FluctuationStrength   = [Psychoacoustics 'FluctuationStrength_Rodrigo' delim];
+    paths.FluctuationStrength_TestTones = [paths.FluctuationStrength 'TestTones' delim];
 end
