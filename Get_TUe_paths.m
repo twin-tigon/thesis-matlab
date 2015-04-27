59ea39c98d9ea924c01d9e0a013e5742e738d892
Rodrigo García <yo@rodrigogarcia.me>
2015-04-27 13:37:11 +0200

    Adds matlab2tikz to Get_TUe_paths

--- Get_TUe_paths.m
+++ Get_TUe_paths.m
@@ -3,9 +3,10 @@ function paths = Get_TUe_paths
     MATLAB_shared       = [MATLAB_base 'MATLAB_shared' delim];
     Psychoacoustics     = [MATLAB_shared 'Psychoacoustics' delim];
 
-    paths.DSP                   = [MATLAB_shared 'DSP' delim];
-%     paths.PsySound32            = [MATLAB_shared 'tb_PsySound32' delim];
+%     paths.PsySound32    = [MATLAB_shared 'tb_PsySound32' delim];
+    paths.DSP           = [MATLAB_shared 'DSP' delim];
+    paths.matlab2tikz   = [MATLAB_shared 'matlab2tikz' delim];
 
-    paths.FluctuationStrength   = [Psychoacoustics 'FluctuationStrength_Rodrigo' delim];
+    paths.FluctuationStrength           = [Psychoacoustics 'FluctuationStrength_Rodrigo' delim];
     paths.FluctuationStrength_TestTones = [paths.FluctuationStrength 'TestTones' delim];
 end
