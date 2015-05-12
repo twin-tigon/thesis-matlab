d45c6812ebe3f629d5fd89a8043a9cfe64ccedaf
Rodrigo García <yo@rodrigogarcia.me>
2015-05-12 20:38:55 +0200

    Adds new paths regarding APEX libraries

--- Get_TUe_paths.m
+++ Get_TUe_paths.m
@@ -3,9 +3,11 @@ function paths = Get_TUe_paths
     MATLAB_shared       = [MATLAB_base 'MATLAB_shared' delim];
     Psychoacoustics     = [MATLAB_shared 'Psychoacoustics' delim];
 
-%     paths.PsySound32    = [MATLAB_shared 'tb_PsySound32' delim];
     paths.DSP           = [MATLAB_shared 'DSP' delim];
     paths.matlab2tikz   = [MATLAB_shared 'matlab2tikz' delim];
+    paths.APEX          = [MATLAB_shared 'tb_APEX' delim];
+    paths.APEXtools     = [paths.APEX 'tools' delim];
+%     paths.PsySound32    = [MATLAB_shared 'tb_PsySound32' delim];
 
     paths.FluctuationStrength           = [Psychoacoustics 'FluctuationStrength_Rodrigo' delim];
     paths.FluctuationStrength_TestTones = [paths.FluctuationStrength 'TestTones' delim];
