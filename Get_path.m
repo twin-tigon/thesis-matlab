7885d25565f823f23b02becd3c2f17b2b7b5998f
Rodrigo García <yo@rodrigogarcia.me>
2015-06-22 11:39:28 +0200

    Adds function to define support paths

---
+++ Get_path.m
@@ -0,0 +1,23 @@
+function path = Get_path(name)
+% function path = Get_path(name)
+% 
+% Function to retrieve support paths that change according to the runtime
+% environment.
+% 
+% Author: Rodrigo García
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+    paths.FluctuationStrengthTones = [ ...
+        '/Users/rodrigo/VirtualBox VMs/Luxuria/share/APEX_shared/stimulus/'
+    ];
+    
+    if ~isfield(paths,name)
+        error('The requested path has not been defined!');
+    end
+    
+    path = paths.(name);
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+end
