4b8021f15d1eee25da674f2d2b3e49b38e0e43a5
Rodrigo García <yo@rodrigogarcia.me>
2015-08-12 14:34:21 +0200

    Fixes error when adding recursive paths

--- Get_TUe_paths.m
+++ Get_TUe_paths.m
@@ -17,15 +17,15 @@ function paths = Get_TUe_paths
     cellfun(@(path)add_path_recursively(path),RECURSIVE_PATHS);
 
     function add_path_recursively(basepath)
-        basename = get_last_part(basepath);
+        basename = get_basename(basepath);
         add_to_paths(basename,basepath);
         
         subpaths = strsplit(genpath(basepath),':');
         subpaths = subpaths(2:end-1);
         
         cellfun(@(path) ...
-            add_to_paths([basename '_' get_last_part(path)],path), ...
-            subpaths);
+            add_to_paths([basename '_' get_last_part(path,basename)]...
+                ,path),subpaths);
     end
 
     function add_to_paths(name,path)
@@ -36,8 +36,15 @@ function paths = Get_TUe_paths
         paths.(name) = [path delim];
     end
 
-    function last = get_last_part(path)
+    function basename = get_basename(path)
+        splitted = strsplit(path,'/');
+        basename = splitted{length(splitted)};
+    end
+
+    function last = get_last_part(path,basename)
         splitted    = strsplit(path,'/');
-        last        = splitted{length(splitted)};
+        splitted    = splitted(2:end);
+        last        = splitted(Stringindex(splitted,basename)+1:end);
+        last        = Celljoin(last,'_');
     end
 end
